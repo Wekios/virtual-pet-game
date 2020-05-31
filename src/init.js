1a43ac9a395e6d185bbf4d5a8b0d5f2745b5e676
Veljko Blagojevic <veljko@geta.no>
2020-05-31 14:53:16 +0200

    adds transitioning states

--- src/init.js
+++ src/init.js
@@ -1,9 +1,9 @@
-import game from "./gameState";
+import game, { handleUserAction } from "./gameState";
 import { TICK_RATE } from "./constants";
 import initButtons from "./buttons";
 
 async function init() {
-  initButtons(game.handleUserAction);
+  initButtons(handleUserAction);
   let nextTimeToTick = Date.now();
 
   function nextAnimationFrame() {
