ffa2e115ca3b68e57b4b598ff4389b0714525621
Wekios <veljkod.blagojevic@gmail.com>
2023-05-13 20:42:19 +0200

    chore: converted project from parcel into vite

--- src/buttons.js
+++ src/buttons.js
@@ -1,9 +1,7 @@
 import { ICONS } from "./constants";
 
 const toggleHighlighted = (icon, show) =>
-  document
-    .querySelector(`.${ICONS[icon]}-icon`)
-    .classList.toggle("highlighted", show);
+  document.querySelector(`.${ICONS[icon]}-icon`).classList.toggle("highlighted", show);
 
 export default function initButtons(handleUserAction) {
   let selectedIcon = 0;
---
+++ vite.config.js
@@ -0,0 +1,7 @@
+export default {
+  root: "src",
+  base: "/virtual-pet-game",
+  build: {
+    outDir: "../dist",
+  },
+};
